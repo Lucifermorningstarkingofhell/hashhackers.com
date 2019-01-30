94c56992d1a08e97cc7631251807547cd7236b3a
Parveen Bhadoo <35890959+ParveenBhadooOfficial@users.noreply.github.com>
2019-01-30 17:20:37 +0530

    Update sw-register.js

--- static/sw-register.js
+++ static/sw-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-	navigator.serviceWorker.register(superpwa_sw.url)
+	navigator.serviceWorker.register(bhadoo_sw.url)
 	.then(function(registration) { console.log('Hash Hackers Service worker ready'); registration.update(); })
 	.catch(function(error) { console.log('Registration failed with ' + error); });
   });
